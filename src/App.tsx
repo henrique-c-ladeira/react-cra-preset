6509b4873e331ec7b09257a76846cc5ab7b67e33
Henrique <henrique.c.ladeira@gmail.com>
2022-02-05 14:42:16 -0300

    feat: remove arquivos não utilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
 
-export default App;
+
+  
+  <div className="App">
+    <header className="App-header">
+      <p>
+        Edit <code>src/App.tsx</code>
+        <code>src/App.tsx</code>
+        <code>src/App.tsx</code>
+        <code about="123" className="123" radioGroup="123" vocab="asd" spellCheck={true}>
+          src/App.tsx
+        </code>
+        <code>
+          src/App.tsxrc/App.tsxrc/App.tsxrc/App.tsxrc/Apprc/App.tsxrc/Apprc/App.tsxrc/Apprc/App.tsxrc/Apprc/App.tsxrc/App.tsxrc/App.tsxrc/App.tsxrc/App.tsxrc/App.tsxrc/App.tsxrc/App.tsx
+        </code>
+        <code>src/App.tsx</code>
+        <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+)
+
+export default App
