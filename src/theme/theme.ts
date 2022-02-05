f9611d879f473269dfd7042a6a9bd22749045305
Henrique <henrique.c.ladeira@gmail.com>
2022-02-05 16:40:33 -0300

    feat: adiciona globalStyle e arquivo de tema

---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: '#112456',
+    primaryText: 'white',
+
+    secondary: 'black',
+    secondaryText: 'white',
+
+    backgroundColor: '#4433552A',
+
+    textBody: '#334455',
+  },
+};
